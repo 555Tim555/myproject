7846004de72b0645c4615b232bde131a18aa37bd
555Tim555 <s10114105@gmail.com>
2020-03-11 21:31:45 +0800

    第3次

--- src/main/kotlin/com/zy/Hello.kt
+++ src/main/kotlin/com/zy/Hello.kt
@@ -2,16 +2,33 @@ package com.zy
 
 fun main() {
     //print("Hello Kotlin")
-    val h = Human().hello()
+    val h = Human("Tim",66.5f,1.7f)
+    println(h.bmi())
 
-    var age=19
+   /* var age=19
     age=20
     var weight = 66.5
     var name : String = "Tang"
-    print(name)
+    print(name)*/
 }
 
-class Human(){
+class Human(var weight : Float, var height : Float){
+
+    init {
+        println("test $weight")
+    }
+
+    constructor(name : String, weight: Float,height: Float) : this(weight,height){
+        println("test2 $name")
+    }
+
+
+
+    fun bmi():Float{
+        val bmi = weight / (height*height)
+        return bmi
+    }
+
     fun hello(){
         println("Hello Kotlin")
     }
