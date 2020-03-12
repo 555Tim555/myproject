06cf5e0e505e1cbc229fdd933dc0b00b4dceaaf7
555Tim555 <s10114105@gmail.com>
2020-03-12 22:02:11 +0800

    第4次

--- src/main/kotlin/com/zy/Student/student.kt
+++ src/main/kotlin/com/zy/Student/student.kt
@@ -1,11 +1,21 @@
 package com.zy.Student
 
+import java.util.*
+
 fun main() {
-    val stu = Student("Tang",80,100)
+    val scanner = Scanner(System.`in`)
+    print("Please enter student name:")
+    var name = scanner.next()
+    print("Please enter student english:")
+    var english = scanner.nextInt()
+    print("Please enter student math:")
+    var math = scanner.nextInt()
+
+    val stu = Student(name,english,math)
     stu.print()
 }
 
-class student(var name : String, var english : Int, var math : Int){
+class Student(var name : String, var english : Int, var math : Int){
     fun print(){
         println(name + "\t" + english + "\t" + math + "\t" + (english+math)/2)
     }
