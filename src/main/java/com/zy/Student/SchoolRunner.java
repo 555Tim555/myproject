01f55986c250181a225c49c9590d4ec357923e59
555Tim555 <s10114105@gmail.com>
2020-03-12 20:43:01 +0800

    第3次

--- src/main/java/com/zy/Student/SchoolRunner.java
+++ src/main/java/com/zy/Student/SchoolRunner.java
@@ -1,7 +1,18 @@
 package com.zy.Student;
 
+import java.util.Scanner;
+
 public class SchoolRunner {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter student name:");
+        String name = scanner.next();
+        System.out.print("Please enter student english:");
+        int english = scanner.nextInt();
+        System.out.print("Please enter student math:");
+        int math = scanner.nextInt();
+
+        //Student stu = new Student(name,english,math);
         Student stu = new Student("Tang",80,100);
         stu.print();
     }
