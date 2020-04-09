62c76fbf9a6ab758e905defaca4994f27fdb7de0
555Tim555 <s10114105@gmail.com>
2020-04-09 22:16:33 +0800

    6

--- src/main/java/com/zy/lottery/Game539.java
+++ src/main/java/com/zy/lottery/Game539.java
@@ -8,13 +8,25 @@ public class Game539 extends NumberGame{
     public void generate() {
         Random random = new Random();
         int rnd;
-        int i=0;
-        do{
-            rnd = random.nextInt(39);
-            numbers.add(rnd);
-            i++;
-        }while(i<5);
-        System.out.println(numbers.size());
+        int flag = 0;
+        while(numbers.size()<5){
+            rnd = random.nextInt(39)+1;
+            for(int n : numbers)
+            {
+                if(rnd == n){
+                    rnd = random.nextInt(39)+1;
+                    numbers.add(rnd);
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag!=1){
+                numbers.add(rnd);
+                flag=0;
+            }
+
+        }
+        //System.out.println(numbers.size());
         for(int number : numbers)
         {
             System.out.print(number+"\t");
